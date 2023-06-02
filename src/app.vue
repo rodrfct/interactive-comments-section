fd21d9f64b55acb79e9001409c5db775c1109f62
rodrfct <rodrigoca.fct@gmail.com>
2023-06-02 11:37:01 +0200

    Initial comment structure

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,27 @@
 <script setup>
+import anImage from './assets/avatars/image-ramsesmiron.png'
 
 useHead({
   title: "Frontend Mentor | Interactive comments section"
 })
+
+// For drafting, should be replaced by a fetch call later
+const currrentUser = {
+    userName: "Florentino"
+}
+
 </script>
 
 <template>
   <div>
-    <Comment />
+    <Comment commentId="1"
+      content="Hello World!"
+      createdAt="2 days ago"
+      score="3"
+      userName="Federico"
+      :userImage="anImage"
+      :currrentUser="currrentUser"
+    />
 
     <CommentInput />
   </div>
--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,3 +1,62 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+    commentId: {
+        type: Number,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: String,
+        required: true
+    },
+    score: {
+        type: Number,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    userImage: {
+        type: String,
+        required: true
+    },
+    replies: {
+        type: Array,
+    },
 
-<template></template>
+    currrentUser: {
+        type: String
+    }
+})
+
+const isMine = props.userName ==  props.currrentUser.userName
+
+</script>
+
+<template>
+    <article class="comment">
+        <div class="score">{{ props.score }}</div>
+
+        <div class="content">
+            <img :src="props.userImage" alt="">
+
+            <span>{{ props.userName }}</span>
+
+            <span  v-if="isMine" class="you">you</span>
+
+            <span>{{ props.createdAt }}</span>
+
+            <div v-if="isMine" class="actions">
+                <button>Delete</button>
+                <button>Edit</button>
+            </div>
+            <div v-else class="actions">
+                <button>Reply</button>
+            </div>
+        </div>
+    </article>
+</template>
